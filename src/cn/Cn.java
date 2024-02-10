3d6fee315aff0a9e658adebda73ddfc034b09977
ash456123 <queenofrani3@gmail.com>
2024-02-10 13:23:12 +0530

    modified

--- src/cn/Cn.java
+++ src/cn/Cn.java
@@ -3,7 +3,8 @@ package cn;
 public class Cn {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("first page");
+		System.out.println("first page");
 
 	}
 
